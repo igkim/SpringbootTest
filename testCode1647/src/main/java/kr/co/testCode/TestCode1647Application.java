0dbd049328d7610bcbed60e9b8e40ee6e08f258b
igkim <m_mode@naver.com>
2024-09-10 15:09:29 +0900

    aaaa

---
+++ testCode1647/src/main/java/kr/co/testCode/TestCode1647Application.java
@@ -0,0 +1,13 @@
+package kr.co.testCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCode1647Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCode1647Application.class, args);
+	}
+
+}
---
+++ testCode1647/src/test/java/kr/co/testCode/TestCode1647ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.testCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCode1647ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
