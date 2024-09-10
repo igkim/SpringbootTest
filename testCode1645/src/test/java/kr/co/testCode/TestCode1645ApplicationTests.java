1e2eb658043d7936bb404ee55c4aa53e7f3cd7b5
henry kim <m_mode@naver.com>
2024-09-10 15:05:38 +0900

    Revert "Revert "First Project""
    This reverts commit 3f3796449f8c3b63fe85ce52e578ca8c4e531425.

---
+++ testCode1645/src/test/java/kr/co/testCode/TestCode1645ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.testCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCode1645ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
