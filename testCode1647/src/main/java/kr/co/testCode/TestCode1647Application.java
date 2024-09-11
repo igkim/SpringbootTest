76a48fb634367c74ea8a2fc436d91a0038c1eb09
henry kim <m_mode@naver.com>
2024-09-11 13:35:41 +0900

    Second push

--- testCode1647/src/main/java/kr/co/testCode/TestCode1647Application.java
+++ testCode1647/src/main/java/kr/co/testCode/TestCode1647Application.java
@@ -1,13 +1,24 @@
 package kr.co.testCode;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
-public class TestCode1647Application {
+public class TestCode1647Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestCode1647Application.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+		log.info("BBBB");
 	}
 
 }
